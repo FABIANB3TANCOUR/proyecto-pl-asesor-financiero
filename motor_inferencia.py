4c06f4012484ca4f9fe1ff6b0fc19be394c2e7de
Sergio Martinez <al22760588@ite.edu.mx>
2025-11-01 18:14:49 -0700

    se agrego el motor de inferencia

---
+++ motor_inferencia.py
@@ -0,0 +1,40 @@
+hechos = (
+    ('gastos', 'fijos', 'renta', 'alta'),
+    ('gastos', 'fijos', 'electricidad', 'alta'),
+    ('gastos', 'fijos', 'telefono/internet', 'media'),
+    ('gastos', 'fijos', 'seguros', 'media'),
+    ('gastos', 'variables', 'comida', 'alta'),
+    ('gastos', 'variables', 'transporte', 'media'),
+    ('gastos', 'variables', 'ropa', 'baja'),
+    ('gastos', 'variables', 'entretenimiento', 'baja'),
+    ('gastos', 'variables', 'intereses_deuda', 'alta')
+)
+
+# Reglas 
+
+def hay_ingreso():
+    return
+
+def cumple_regla_50_30_20():
+    return
+
+def requieres_ajuste():
+    return
+
+def tiempo_para_ahorrar():
+    return
+
+def puede_ahorrar():
+    return
+
+def existen_gastos_prioridad_alta():
+    return
+
+def calcular_deficit():
+    return
+
+def cumple_porcentaje_objetivo():
+    return
+
+def calcular_monto_recorte():
+    return
