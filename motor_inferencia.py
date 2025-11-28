ad89951ed051fbdff7123401016219bdb4a8d0cf
FabianBetancourtFernandez <al22760578@ite.edu.mx>
2025-11-28 00:35:05 -0800

    base de conocimiento separada del motor

--- motor_inferencia.py
+++ motor_inferencia.py
@@ -1,39 +1,5 @@
 import math
-hechos = (
-    ('gastos', 'fijos', 'renta', 'alta'),
-    ('gastos', 'fijos', 'agua', 'alta'),
-    ('gastos', 'fijos', 'electricidad', 'alta'),
-    ('gastos', 'fijos', 'telefono/internet', 'media'),
-    ('gastos', 'fijos', 'seguros', 'media'),
-    ('gastos', 'variables', 'comida', 'alta'),
-    ('gastos', 'variables', 'transporte', 'media'),
-    ('gastos', 'variables', 'ropa', 'baja'),
-    ('gastos', 'variables', 'entretenimiento', 'baja'),
-    ('gastos', 'variables', 'intereses deuda', 'alta'),
-    ('objetivo', .50, ('alta', 'media')),
-    ('objetivo', .30, ('baja',))
-)
-
-'''
-ej_datos = {
-    'ingresos': 1000,
-    'gastos': {
-        'fijos': [
-            {'renta': 200},
-            {'electricidad': 100},
-            {'telefono/internet': 100},
-            {'seguros': 100}
-            ],
-        'variables': [
-            {'comida': 50},
-            {'transporte': 50},
-            {'ropa': 250},
-            {'entretenimiento': 100}
-        ]
-    },
-    'meta_ahorro': 10000
-}
-'''
+from base_conocimiento import hechos
 
 # funciones auxiliares
 def sumar_gastos(gastos:dict, tipos=('fijos', 'variables'), tipo_index=0, gasto_index=0, total=0):
